becae446ba78b83b22ca577fe3d0418637fdd4e7
mohitarya <mohitarya8@gmail.com>
2016-03-22 01:10:00 +0530

    Added code for print circular array, not fully completed

---
+++ interview_bit/arrays/circular_print/circular_print.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_arr(int **, int, int);
+
+int main()
+{
+    int **arr;
+    int rows, columns, i, j, number = 0, ret;
+    printf("Input Number of rows\t");
+    scanf("%d", &rows);
+    printf("Input Number of column\t");
+    scanf("%d", &columns);
+    arr = (int **) malloc(sizeof(int *) * rows);
+    if (!arr) {
+        printf("Error in allocating the memory\n");
+        ret = -1;
+        goto error;
+    }
+    for(i = 0; i < rows; i++){
+        *(arr + i) = (int * ) malloc(sizeof(int) * columns);
+        if(!(*(arr + i))){
+            ret = -1;
+            goto error;
+        }
+    }
+    for (i = 0; i < rows; i++){
+        for (j = 0; j < columns; j++){
+            *(*(arr + i) + j) = number++;
+        }
+    }
+    print_arr(arr, rows, columns);
+    ret = 0;
+
+error:
+    if(arr){
+        for(i = 0; i < rows; i++){
+            if(*(arr + i)){
+                free(*(arr + i));
+            }
+        }
+        free(arr);
+    }
+return 0;
+}
+
+void print_arr(int **arr, int rows, int columns)
+{
+    int i, j;
+    printf("Two D Array is:\n");
+    for(i = 0; i < rows; i++){
+        for(j = 0; j < columns; j++){
+            printf("%d\t", *(*(arr + i) + j));
+        }
+        printf("\n");
+    }
+}
