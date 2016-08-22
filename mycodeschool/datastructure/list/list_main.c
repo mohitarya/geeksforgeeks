c0762406ad51d7c9256b427e89a5214722ab2468
mohitarya <mohitarya8@gmail.com>
2016-08-22 11:06:05 +0530

    Started with list implementation

---
+++ mycodeschool/datastructure/list/list_main.c
@@ -0,0 +1,24 @@
+/* Description: main file for create a list
+ *
+ * Author: Mohit Arya
+ */
+
+/*Name: main.c
+ *
+ * Description: Entry point to the create a list
+ *
+ * Arguments: None
+ *
+ * Return: 0 on success
+ *
+ * Author: Mohit Arya
+ *
+ */
+
+int main(void)
+{
+    while(1){
+        
+    }
+    return 0;
+}
