99a8c1653564a1cc9d43c78e1483d3245e52b699
mohitarya <mohitarya8@gmail.com>
2016-09-22 11:52:31 +0530

    Added code to find min and max in a tree

--- mycodeschool/trees/bst/simple_bst/simple_bst.c
+++ mycodeschool/trees/bst/simple_bst/simple_bst.c
@@ -12,21 +12,52 @@ typedef struct bst_node {
 node *insert(node *, int);
 node *create_node(int);
 void search_data(node *, int);
-
+int find_min(node *);
 // Main Starts from here
 
 int main()
 {
+    int min_val, max_val;
     node *root = NULL;
     root = insert(root, 15);
     root = insert(root, 10);
     root = insert(root, 20);
     root = insert(root, 5);
-    search_data(root, 20);
-    search_data(root, 50);
+    //search_data(root, 20);
+    //search_data(root, 50);
+    min_val = find_min(root);
+    max_val = find_max(root);
+    printf("Min Value in tree is %d\n", min_val);
+    printf("Max value in tree is %d\n", max_val);
     return 0;
 }
 
+int find_max(node *root)
+{
+    if(root->right == NULL){
+        return root->data;
+    }
+    int val;
+    val = find_min(root->right);
+    if(val > root->data){
+        return val;
+    }else{
+        return root->data;
+    }
+}
+int find_min(node *root)
+{
+    if(root->left == NULL){
+        return root->data;
+    }
+    int val;
+    val = find_min(root->left);
+    if(val < root->data){
+        return val;
+    }else{
+        return root->data;
+    }
+}
 void search_data(node *root, int data)
 {
     if(root == NULL){
