41b35319fa98ae6706d9911f5d04f97f5f63e6a0
root <root@localhost.localdomain>
2017-01-28 11:04:51 -0800

    Added insertion sort following book introduction to algorithms problem-2.1

---
+++ algos/problem-2.1/insertion_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void print_arr(int *ptr, int size)
+{
+    int i;
+    printf("Array is:::\n\t\t");
+
+    for(i = 0; i < size; i++){
+        printf("%d\t", *(ptr + i));
+    }
+    printf("\n");
+}
+
+void insertion_sort(int *ptr, int size)
+{
+    int i, j, key;
+    for(i = 1; i < size; i++)
+    {
+        key = *(ptr + i);
+        for(j = i - 1; j >= 0; j--){
+            if(*(ptr + j) > key){
+                *(ptr + j + 1) = *(ptr + j);
+                continue;
+            }else{
+                break;
+            }
+        }
+        *(ptr + j + 1) = key;
+    }
+    printf("Sorted ");
+    print_arr(ptr, size);
+}
+
+int main()
+{
+    int arr[] = {7, 2, 0, 1, 8, 3};
+    int arr_size = sizeof(arr)/sizeof(int);
+    print_arr(arr, arr_size);
+    insertion_sort(arr, arr_size);
+}
