86693238a8f3b0d5308beab680e81fbd33b38adc
mohitarya <mohitarya8@gmail.com>
2017-01-29 09:48:50 -0800

    Book-ITL, Chapter-2, Problem-2.1,2.1-2, Insersion Sort

--- algos/chapter-2/problem-2.1/insertion_sort.c
+++ algos/chapter-2/problem-2.1/insertion_sort.c
@@ -11,6 +11,26 @@ void print_arr(int *ptr, int size)
     printf("\n");
 }
 
+// Insertion sort descending order
+void insertion_sort_desc(int *ptr, int size)
+{
+    int i, j, key;
+    for(i = 1; i < size; i++){
+        key = *(ptr + i);
+        for(j = i - 1; j >= 0; j--){
+            if(*(ptr + j) < key){
+                *(ptr + j + 1) = *(ptr + j);
+            }else{
+                break;
+            }
+        }
+        *(ptr + j + 1) = key;
+    }
+    printf("Sorted descending order ");
+    print_arr(ptr, size);
+}
+
+// Insertion sort ascending order
 void insertion_sort(int *ptr, int size)
 {
     int i, j, key;
@@ -37,4 +57,5 @@ int main()
     int arr_size = sizeof(arr)/sizeof(int);
     print_arr(arr, arr_size);
     insertion_sort(arr, arr_size);
+    insertion_sort_desc(arr, arr_size);
 }
