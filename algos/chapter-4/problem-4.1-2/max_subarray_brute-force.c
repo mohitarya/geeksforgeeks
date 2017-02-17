d27a330776933171cda0b21fd1886808cdc67196
root <root@localhost.localdomain>
2017-02-18 00:40:43 +0530

    Made more efficient in space complexity and returning the index also with values

--- algos/chapter-4/problem-4.1-2/max_subarray_brute-force.c
+++ algos/chapter-4/problem-4.1-2/max_subarray_brute-force.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 #include <limits.h>
+#include <string.h>
+#include <stdlib.h>
 
+int *max_array_using_brute_force(int *arr, int size)
+{
+    int *result;
+    int i, j, max_array=INT_MIN, sum;
+    result = (int *)malloc(sizeof(int) * 3);
+    memset(result, -1, (sizeof(int) * 3));
+    for (i = 0; i < size; i++){
+        sum = 0;
+        for(j = i; j < size; j++){
+            sum = sum + arr[j];
+            if(max_array < sum){
+                max_array = sum;
+               result[0] = i;
+               result[1] = j;
+            }
+        }
+    }
+    result[2] = max_array;
+    return result;
+}
 int main()
 {
     int arr[] = {10, -12, 3, 8, -2};
-    int result, size;
+    int *result, size;
     size = sizeof(arr)/sizeof(int);
     result = max_array_using_brute_force(arr, size);
     printf("Input array is ::\n\t\t");
     print(arr, size);
-    printf("Maximum array is %d\n", result);
+    printf("Maximum array sum-%d at start index-%d end index-%d,\n", result[2], result[0], result[1]);
     return 0;
 }
 
-int max_array_using_brute_force(int *arr, int size)
-{
-    int i, j, max_array=INT_MIN, prev_sum, sum;
-    for (i = 0; i < size; i++){
-        prev_sum = 0;
-        for(j = i; j < size; j++){
-            sum = prev_sum + arr[j];
-            if(max_array < sum){
-                max_array = sum;
-            }
-            prev_sum = sum;
-        }
-    }
-    return max_array;
-}
